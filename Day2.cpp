a8bd7ea111e4f32791260ed6fa7f2ccedf1bdabd
akashbytecode <akashchakravarty741@gmail.com>
2026-05-03 16:20:15 +0530

    added day2 code

---
+++ Day2.cpp
@@ -0,0 +1,20 @@
+//check palindrome
+#include<iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+    string rev="";
+    for(int i=str.length()-1;i>=0;i--)
+    {
+        rev+=str[i];
+    }
+    if(str==rev)
+        cout<<"The string is a palindrome."<<endl;
+    else
+        cout<<"The string is not a palindrome."<<endl;
+    return 0;
+}
